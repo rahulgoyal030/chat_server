187e750b4abef3f9b947e409b3273af739bc7d61
rahulgoyal030 <goyal.rahul030@gmail.com>
2015-08-19 01:23:58 +0530

    files added

---
+++ server.js
@@ -0,0 +1,56 @@
+var express= require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.use(express.static(__dirname+ '/public'));
+app.get('/', function(req, res){
+  res.sendfile(__dirname + '/index.html');
+});
+
+var user={};
+
+io.sockets.on('connection', function(socket){
+	console.log(" user is connected");
+
+
+  socket.on('new user', function(data,callback){
+
+     if(data in user)
+      return false;
+     else
+     {
+
+          console.log("we are here");
+          
+          socket.nickname=data;
+
+          user[socket.nickname]= socket;
+
+          console.log(" socket name" , socket.nickname);
+          for(x in user)
+          {
+             console.log(x);
+          }
+     }
+     callback(true);
+    
+  })
+
+    
+
+    socket.on('chat message', function(data){
+
+    console.log(" in server  socket.on" , socket.nickname , "  data is  " , data);
+
+          io.sockets.emit('new message', {nickname: socket.nickname , msg:data});
+
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
